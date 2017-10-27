53de53e6ee62c95aad8d0f9a1ea460e5fafe9871
BrainKub <fyhtjnt@gmail.com>
2017-10-28 02:16:05 +0300

    add build.sh

---
+++ kernel-3.18.19/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+pause()
+{
+read -s -n 1 -p "Press any key to continue...";
+}
+build()
+{
+#Compile kernel with a build script to make things simple
+mkdir -p out
+export CROSS_COMPILE=/home/brainkub/Android/utility/arm-eabi-4.8/bin/arm-eabi-
+export USE_CCACHE=1
+export ARCH=arm ARCH_MTK_PLATFORM=mt6580
+make -C /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19 O=/home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out ARCH=arm Z00YD_defconfig
+#Edit the number according to the number of CPUs you have in your PC:
+make -j4 -C /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19 O=/home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out ARCH=arm
+next;
+}
+pack()
+{
+cp /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out/arch/arm/boot/zImage-dtb /home/brainkub/Android/utility/CarlivImageKitchen64/boot/boot.img-kernel
+cd /home/brainkub/Android/utility/CarlivImageKitchen64/
+./carliv
+}
+next()
+{
+echo "                                                 ";
+echo "                                                 ";
+echo "---------------------------------------------------";
+echo " 1. - Rebuild                                    ";
+echo "---------------------------------------------------";
+echo "  2. - Pack                        ";
+echo "---------------------------------------------------";
+printf %s "Type the number, then press ENTER: ";
+read renv;
+if [ "$renv" == "1" ];
+    then
+      build;
+fi;
+if [ "$renv" == "2" ];
+    then
+      pack;
+fi;
+}
+while :
+do
+	build;
+done
