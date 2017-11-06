bd68282cc46508292107e153f4f3e26b741bef40
BrainKub <fyhtjnt@gmail.com>
2017-11-06 18:52:12 +0300

    Fix Volume Buttons

--- kernel-3.18.19/build.sh
+++ kernel-3.18.19/build.sh
@@ -6,13 +6,14 @@ read -s -n 1 -p "Press any key to continue...";
 build()
 {
 #Compile kernel with a build script to make things simple
+rm -R /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out/
 mkdir -p out
 export CROSS_COMPILE=/home/brainkub/Android/utility/arm-eabi-4.8/bin/arm-eabi-
 export USE_CCACHE=1
 export ARCH=arm ARCH_MTK_PLATFORM=mt6580
 make -C /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19 O=/home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out ARCH=arm Z00YD_defconfig
 #Edit the number according to the number of CPUs you have in your PC:
-make -j4 -C /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19 O=/home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out ARCH=arm
+make -j4 -C /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19 O=/home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out ARCH=arm | tee build.log
 next;
 }
 pack()
