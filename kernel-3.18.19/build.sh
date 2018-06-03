e0f59fc7c9c1503e3a8e8a086fa27ca88171b7d9
BrainKub <fyhtjnt@gmail.com>
2018-06-03 22:37:44 +0300

    Add GT9XX touchscreen driver

--- kernel-3.18.19/build.sh
+++ kernel-3.18.19/build.sh
@@ -6,20 +6,20 @@ read -s -n 1 -p "Press any key to continue...";
 build()
 {
 #Compile kernel with a build script to make things simple
-rm -R /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out/
+rm -R /home/brainkub/Android/kernel/Z00YD/kernel-3.18.19/out/
 mkdir -p out
 export CROSS_COMPILE=/home/brainkub/Android/utility/arm-eabi-4.8/bin/arm-eabi-
 export USE_CCACHE=1
 export ARCH=arm ARCH_MTK_PLATFORM=mt6580
-make -C /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19 O=/home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out ARCH=arm Z00YD_defconfig
+make -C /home/brainkub/Android/kernel/Z00YD/kernel-3.18.19 O=/home/brainkub/Android/kernel/Z00YD/kernel-3.18.19/out ARCH=arm Z00YD_defconfig
 #Edit the number according to the number of CPUs you have in your PC:
-make -j4 -C /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19 O=/home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out ARCH=arm | tee build.log
+make -j4 -C /home/brainkub/Android/kernel/Z00YD/kernel-3.18.19 O=/home/brainkub/Android/kernel/Z00YD/kernel-3.18.19/out ARCH=arm | tee build.log
 next;
 }
 pack()
 {
-cp /home/brainkub/Android/kernel/GITHUB/kernel-3.18.19/out/arch/arm/boot/zImage-dtb /home/brainkub/Android/utility/CarlivImageKitchen64/boot/boot.img-kernel
-cd /home/brainkub/Android/utility/CarlivImageKitchen64/
+cp /home/brainkub/Android/kernel/Z00YD/kernel-3.18.19/out/arch/arm/boot/zImage-dtb /home/brainkub/Android/utility/carliv/boot/boot.img-kernel
+cd /home/brainkub/Android/utility/carliv/
 ./carliv
 }
 next()
